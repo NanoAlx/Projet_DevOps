cbd7b3dc241d96318e33cb5ea16d5c9fd114ff2f
Ruben Woliner <113244262+Rubz8@users.noreply.github.com>
2024-01-16 09:57:14 +0100

    Delete janvier_script.py

--- janvier_script.py
+++
@@ -1,8 +0,0 @@
-# Utilisation de Python pour reproduire la condition
-janvier_journal = False  # Remplacez False par la valeur que vous souhaitez tester
-
-if janvier_journal:
-    print("Confirmation des US pour Janvier")
-    print(f"Journal intime: {janvier_journal}")
-else:
-    print("Journal intime non confirmé pour Janvier")
